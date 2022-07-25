fed7178c1cff51de5e8cd916c38aa250cd297afe
hamed nasr <hmd.nasr@gmail.com>
2022-07-24 23:33:28 -0400

    ethers imported

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { ethers } from "./ethers-5.6.esm.js";
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -6,4 +8,8 @@ async function connect() {
     document.getElementById("connectButton").innerHTML =
       "MetaMask is not installed!";
   }
+}
+
+async function fund(ethAmount) {
+  console.log();
 }
